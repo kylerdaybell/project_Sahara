10369bf423ee7f6540312ccf537a15ffd48aef02
kyler <kyler@pop-os.localdomain>
2019-09-28 18:33:03 -0600

    the banner now displays incorrect password only when the password is incorrect

--- node/controllers/LoginController.js
+++ node/controllers/LoginController.js
@@ -2,20 +2,20 @@ var UserModel = require('./../models/UserModel');
 
 var LoginController ={
     getroot: function(res,req){
-        return res.render('index',{title:"project sahara"});
+        return res.render('index',{title:"project sahara",display:false});
     },
     postlogin: function(res,req){
         if(UserModel.login(req.body.username,req.body.password)){
             return res.render('home');
         }else{
-            return res.render('index',{title:"bad login"});
+            return res.render('index',{title:"your username or password is incorrect",display:true});
         }
     },
     postRegister: function(res,req){
-        if( "newusercreated"==UserModel.Register(res.body.username,res.body.password,res.body.confirm)){
-            return res.render('index',{title:""});
+        if("newusercreated"==UserModel.Register(res.body.username,res.body.password,res.body.confirm)){
+            return res.render('index',{title:"",display:false});
         }else{
-            return res.render('register',{title:"failed to create username"});
+            return res.render('register',{title:"failed to create username",display:true});
         }
     }
 }
