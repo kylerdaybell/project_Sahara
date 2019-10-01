e142c3fd409cf0ff2685f0757c5fc0f9e813ad02
kyler <kyler@pop-os.localdomain>
2019-10-01 11:40:58 -0600

    tests have been written for the add new catagory

--- node/tests/CategoryModel.test.js
+++ node/tests/CategoryModel.test.js
@@ -2,12 +2,17 @@ var SaharaTestService = require('../services/SaharaTestService');
 var CategoryModel = require('../models/CategoryModel');
 CategoryModel.constructor(SaharaTestService);
 
-test('Given a valid username and the user has created a category before the category model should return at least one category',()=>{
-    categories = CategoryModel.getAllcategories('kyler');
+test('Given a valid username and the user has created a category before the category model should return at least one category',async ()=>{
+    categories = await CategoryModel.getAllCategories('kyler');
     expect(categories.length).toBeGreaterThan(0);
 })
 
-test('Given an invalid username the Category Model should return an empty list',()=>{
-    categories = CategoryModel.getAllcategories('bob');
+test('Given an invalid username the Category Model should return an empty list',async ()=>{
+    categories = await CategoryModel.getAllCategories('bob');
     expect(categories.length).toBeLessThanOrEqual(0);
+})
+
+test('Given a user is logged in that user can add catagories to the database',async ()=>{
+    WasCatagoryAdded = await CategoryModel.addNewCategory('username','catagory name','catagory discription','color');
+    expect(WasCatagoryAdded).toBe(true);
 })
