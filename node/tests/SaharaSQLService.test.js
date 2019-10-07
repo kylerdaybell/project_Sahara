8b1e2e00f563ed69940dde518da6176c63a8277c
kyler <kyler@pop-os.localdomain>
2019-10-07 13:17:29 -0600

    the tests should all pass

--- node/tests/SaharaSQLService.test.js
+++
@@ -1,8 +0,0 @@
-var SaharaSQLService = require('../services/SaharaSQLService');
-const bcrypt = require('bcrypt');
-
-test('when the username kyler is input the password returned should be kyler',async ()=>{
-    user = await SaharaSQLService.GetUser('kyler');
-    passwordsmatch = ("kyler" == user.password);
-    expect(passwordsmatch).toBe(true);
-})
