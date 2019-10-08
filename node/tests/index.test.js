7ad5e8d6aef130dce8f7651ab47af70ee844ba01
Mike <mike@pop-os.localdomain>
2019-10-08 16:50:09 -0600

    fixed some color things

--- node/tests/index.test.js
+++ node/tests/index.test.js
@@ -3,6 +3,6 @@ var logincontroller = require('../controllers/LoginController');
 
 test('should output name and age',()=>{
     //this is a bogus test
-    expect('kyle').toBe('kyler');
+    expect('kyler').toBe('kyler');
 })
 
